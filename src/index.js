3fd8873a57bd1842bae70b50466365048af508f9
apricotn <56545404+apricotn18@users.noreply.github.com>
2023-05-25 19:24:42 +0900

    230525

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import './assets/reset.css';
 import './assets/common.css';
 import Home from './Home/Home';
